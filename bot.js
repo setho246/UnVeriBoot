57c910567773e6208ab8dde2cafc7f7879b65c6a
setho246 <setho246@gmaail.com>
2019-06-11 19:58:05 +1000

    created staging

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-	console.log('These boots were made for bootin');
+	console.log('These boots were made for bootin`');
 });
 
-client.login("***REMOVED***");
+client.login("***REMOVED***"); ````
