ec07e62ccb82f5fba10516730cec23972b03e693
setho246 <setho246@gmaail.com>
2019-06-11 18:34:02 +1000

    Inital Commit

---
+++ bot.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+	console.log('These boots were made for bootin');
+});
+
+client.login("NTg3ODkzNTMzNzQzMTIwMzk0.XP9Neg.k5Gju6FaJMGhoEhLZ-9qel0CAo8");
